d57eb78f79dbaac297cd76527f516c32d73fa962
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-21 11:21:46 +0100

    chart styled

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../widgets/chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -19,8 +20,18 @@ class Chart extends StatelessWidget {
           totalSum += weekTransactions[i].amount;
         }
       }
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+        0.0,
+        (previousValue, element) =>
+            previousValue += (element['amount'] as double));
   }
 
   @override
@@ -28,8 +39,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
-      child: Row(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                spendingAmount: data['amount'] as double,
+                spendingPctOfTOtal: totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+                label: data['day'] as String,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTOtal;
+
+  const ChartBar(
+      {super.key,
+      required this.spendingAmount,
+      required this.spendingPctOfTOtal,
+      required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FittedBox(
+          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                color: const Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(10),
+              )),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTOtal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
